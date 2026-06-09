7505cd9eae12a4d4ba27d1b800f95de9d39f2be1
Samrat Pratap Sengar <samratsengar11@gmail.com>
2026-06-09 22:54:16 +0530

    Refactor maxProfit method for better clarity

--- Week_1/Day_1/Best_Time_To_buy_And_Sell.java
+++ Week_1/Day_1/Best_Time_To_buy_And_Sell.java
@@ -1,15 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int profit=0;
-        int i=0;
-        int buy=prices[i];
-         int indx1=-1;
-         int indx2=-1;
-         int maxprofit = 0;
-        for(i=0;i<n;i++){
-            if(prices[i]<=buy) {
-                buy=prices[i];
-                indx1 =i;
+        int Maxpro=0;
+        int BestBuy=prices[0];
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]>BestBuy){
+                Maxpro=Math.max(Maxpro,prices[i]-BestBuy);
             }
+            BestBuy=Math.min(BestBuy,prices[i]);
+        }
+
+         
+         
+
+        
+        
+        
+     return Maxpro;}
+}
             
